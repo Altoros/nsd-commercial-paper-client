e020a20eabbac7e85e250187616e5ea79defedad
Oleg Abdrashitov <olegabu73@gmail.com>
2017-09-21 08:39:15 -0700

    p7s extension

--- parse.sh
+++ parse.sh
@@ -4,5 +4,5 @@ filename=$1
 
 cat ${filename} | jq -r .alamedaFrom > "$filename-alamedaFrom.xml"
 cat ${filename} | jq -r .alamedaTo > "$filename-alamedaTo.xml"
-cat ${filename} | jq -r .alamedaSignatureFrom | base64 -d - > "$filename-alamedaSignatureFrom.xml"
-cat ${filename} | jq -r .alamedaSignatureTo | base64 -d - > "$filename-alamedaSignatureTo.xml"
+cat ${filename} | jq -r .alamedaSignatureFrom | base64 -d - > "$filename-alamedaSignatureFrom.p7s"
+cat ${filename} | jq -r .alamedaSignatureTo | base64 -d - > "$filename-alamedaSignatureTo.p7s"
